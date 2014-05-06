44aa7279d86ebfaa0b208e615f34c8d8d23eb0b0
foulo005 <nicolas.foulon2@hotmail.fr>
2014-05-06 17:16:38 +0200

    we can see the accounts of a user.

--- user/AccountOverview.aspx.cs
+++ user/AccountOverview.aspx.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class user_AccountOverview : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        SqlDataSource1.SelectParameters.Add("UserName", Page.User.Identity.Name.ToString());
 
+       
     }
 }
